a3696e877b79c94247c74ecbeacee8dc48cfb6f2
tl <tl@cat.pm>
2019-05-12 12:55:51 +0300

    aikataulu: merkkaa haastattelut/harjoitusket

--- public/extra/aikataulu.js
+++ public/extra/aikataulu.js
@@ -7,14 +7,23 @@ const renderXsumo = event => live.timetable.renderEvent({
 
 const renderRescue = event => live.timetable.renderEvent({
 	clazz: "live-timetable-res",
-	badge: { icon: "/rescue-logo.svg", text: "Rescue" },
+	badge: {
+		icon: "/rescue-logo.svg",
+		text: event.block.id.includes("haast") ? "Rescue (haastattelu)" : "Rescue"
+	},
 	teams: event.teams,
 	arena: event.arena
 });
 
 const renderDance = event => live.timetable.renderEvent({
 	clazz: "live-timetable-dance",
-	badge: { icon: "/tanssi-logo.svg", text: "Tanssi/Teatteri" },
+	badge: {
+		icon: "/tanssi-logo.svg",
+		text:
+			event.block.id.includes("haast") ? "Tanssi (haastattelu)" :
+			event.block.id.includes("harj") ? "Tanssi (harjoitus)" :
+			"Tanssi/Teatteri"
+	},
 	teams: event.teams,
 	arena: event.arena
 });
@@ -30,12 +39,6 @@ live.carousel([
 		components: [
 			live("timetable", "#timetable", {
 				interval: 10000,
-				/*
-				blocks: [
-					"rescue1.1", "rescue1.2",
-					"tanssi.alkeis.esitys", "tanssi.jatko.esitys"
-				],
-				*/
 				rules: [
 					event => event.block.id.startsWith("xs") && renderXsumo,
 					event => event.block.id.startsWith("rescue") && renderRescue,
